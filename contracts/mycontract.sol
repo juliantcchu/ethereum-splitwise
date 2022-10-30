ef2124816ffbf3e1f744701a0a283c561faf21d8
juliantcchu <juliantcchu@gmail.com>
2022-10-30 16:38:21 -0700

    with alternatve contract commented

--- contracts/mycontract.sol
+++ contracts/mycontract.sol
@@ -29,3 +29,51 @@ contract Splitwise {
     }
 
 }
+
+
+
+
+// contract Splitwise {
+// // DO NOT MODIFY ABOVE THIS
+// // ADD YOUR CONTRACT CODE BELOW
+
+//     mapping(address => mapping(address => uint32)) public IOUs;
+
+//     mapping(address => address[]) public creditors;
+
+//     function lookup(address debtor, address creditor) public view returns (uint32 ret) {
+//         return IOUs[debtor][creditor];
+//     }
+
+//     function add_IOU(address creditor, uint32 amount, uint32[] memory cycle, 
+//                         uint32 cycleAmount, bool addCreditor) public {
+
+
+//         // add IOU
+//         IOUs[msg.sender][creditor] = amount;
+//         if (addCreditor){
+//             creditors[msg.sender].push(creditor);
+//         }
+
+//         //resolve cycle if exist
+//         address currDebtor = msg.sender;
+//         for (uint32 i; i < cycle.length; i++){
+//             address currCreditor = creditors[currDebtor][cycle[i]];
+
+//             require(IOUs[currDebtor][currCreditor] >= cycleAmount);
+//             IOUs[currDebtor][currCreditor] -= cycleAmount;
+            
+//             // remove from creditor list
+//             if (IOUs[currDebtor][currCreditor] == 0){
+//                 if (creditors[currDebtor].length - 1 != cycle[i]){
+//                     //put last element in place of curr elem
+//                     creditors[currDebtor][cycle[i]] = creditors[currDebtor][creditors[currDebtor].length - 1];
+//                 }
+//                 creditors[currDebtor].pop();
+//             }
+
+//             currDebtor = creditor;
+//         }
+//     }
+
+// }
